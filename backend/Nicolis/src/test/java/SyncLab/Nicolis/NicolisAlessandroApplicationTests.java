6850e64a3f7ff264a55aa93d3ea539b06caea743
alenico04 <94850914+alenico04@users.noreply.github.com>
2024-07-03 17:55:09 +0200

    baseBackend

---
+++ backend/Nicolis/src/test/java/SyncLab/Nicolis/NicolisAlessandroApplicationTests.java
@@ -0,0 +1,13 @@
+package SyncLab.Nicolis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicolisAlessandroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
