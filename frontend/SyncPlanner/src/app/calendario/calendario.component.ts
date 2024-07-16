c77107a86af8e1f25cc7d2b52cfccbddf041e926
xFire303 <85289871+xFire303@users.noreply.github.com>
2024-07-16 11:40:18 +0200

    fix css and calendar

--- frontend/SyncPlanner/src/app/calendario/calendario.component.ts
+++ frontend/SyncPlanner/src/app/calendario/calendario.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { RouterOutlet, RouterModule } from '@angular/router';
 
@@ -7,27 +7,52 @@ import { FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import { AddPrenotazioneComponent } from "../add-prenotazione/add-prenotazione.component";
+import { AddPrenotazioneComponent } from '../add-prenotazione/add-prenotazione.component';
+
+import { UserService } from '../user.service';
+import { PrenotazioniService } from '../profile/prenotazioni.service';
 
 @Component({
   selector: 'app-calendario',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, FullCalendarModule, AddPrenotazioneComponent],
+  imports: [
+    RouterModule,
+    RouterOutlet,
+    FullCalendarModule,
+    AddPrenotazioneComponent,
+  ],
   templateUrl: './calendario.component.html',
   styleUrl: './calendario.component.css',
 })
-export class CalendarioComponent {
+export class CalendarioComponent implements OnInit {
+  username: string = '';
+
+  prenotazioniList: any[] = [];
+
+  constructor(
+    private userService: UserService,
+    private prenotazioniService: PrenotazioniService
+  ) {
+    this.username = this.userService.getUserUsername();
+  }
+
+  ngOnInit(): void {
+    this.prenotazioniService.getAllPrenotazioni().subscribe((prenotazioni) => {
+      this.prenotazioniList = prenotazioni;
+      this.updateCalendarEvents();
+    });
+  }
+
   showAddPrenotazione: boolean = false;
   selectedDate: string = '';
 
   calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',
+    headerToolbar: {
+      end: 'today,prevYear,prev,next,nextYear'
+    },
     plugins: [dayGridPlugin, interactionPlugin],
     dateClick: (arg) => this.apriPrenotazione(arg),
-    events: [
-      { title: 'event 1', date: '2024-07-01' },
-      { title: 'event 2', date: '2024-07-02' },
-    ],
+    events: [],
   };
 
   apriPrenotazione(arg: any) {
@@ -37,5 +62,13 @@ export class CalendarioComponent {
 
   chiudiPrenotazione() {
     this.showAddPrenotazione = false;
+  }
+
+  private updateCalendarEvents() {
+    this.calendarOptions.events = this.prenotazioniList.map(prenotazione => ({
+      id: prenotazione.id,
+      title: `Prenotazione sede: ${prenotazione.sede}`,
+      start: prenotazione.data,
+    }));
   }
 }
