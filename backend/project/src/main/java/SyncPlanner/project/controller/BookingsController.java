7a7339917cb84fb7adf3c38e940de3b9a099ffc0
Marco Bosco <85289871+xFire303@users.noreply.github.com>
2024-11-29 15:31:08 +0100

    implementazione bot discord

--- backend/project/src/main/java/SyncPlanner/project/controller/BookingsController.java
+++ backend/project/src/main/java/SyncPlanner/project/controller/BookingsController.java
@@ -4,12 +4,14 @@ import SyncPlanner.project.dto.BookingsRequest;
 import SyncPlanner.project.dto.UpdateBooking;
 import SyncPlanner.project.entity.BookingsModel;
 import SyncPlanner.project.service.BookingsService;
+import SyncPlanner.project.service.DiscordBotService;
 import SyncPlanner.project.service.JWTService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -20,9 +22,35 @@ public class BookingsController {
     @Autowired
     private JWTService jwtService;
 
+    @Autowired
+    private DiscordBotService discordBotService;
+
+    private static final Map<String, String> SEDE_TO_DISCORD_ROLE = Map.of(
+            "verona", "1308145201352671302", // ID ruolo per Verona
+            "padova", "1308145246487445555", // ID ruolo per Padova
+            "milano", "345678901234567890", // ID ruolo per Milano
+            "como", "456789012345678901",   // ID ruolo per Como
+            "napoli", "567890123456789012", // ID ruolo per Napoli
+            "roma", "678901234567890123"    // ID ruolo per Roma
+    );
+
     @PostMapping("/bookings")
     public void addBooking(@RequestBody BookingsRequest booking, HttpServletRequest request) {
         String token = jwtService.extractTokenFromHeader(request);
+        String discordRoleId = SEDE_TO_DISCORD_ROLE.get(booking.getSedeName().toLowerCase());
+
+        if (discordRoleId != null) {
+            String message = "Ciao <@&%s>, **%s** sarà in sede il %s".formatted(
+                    discordRoleId,
+                    booking.getUserUsername(),
+                    booking.getDate()
+            );
+
+            discordBotService.sendMessageToThread(message);
+        } else {
+            throw new IllegalArgumentException("Sede non valida: " + booking.getSedeName());
+        }
+
         bookingsService.addBooking(booking);
     }
 
---
+++ backend/project/src/main/java/SyncPlanner/project/service/DiscordBotService.java
@@ -0,0 +1,46 @@
+package SyncPlanner.project.service;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DiscordBotService {
+
+    private JDA jda;
+
+    @Value("${discord.channelId}")
+    private String channelId;
+
+    @Value("${discord.threadId}")
+    private String threadId;
+
+    @Autowired
+    public void DiscordService(JDA jda) {
+        this.jda = jda;
+    }
+
+    public void sendMessageToThread(String message) {
+        TextChannel channel = jda.getTextChannelById(channelId);
+        if (channel != null) {
+            ThreadChannel thread = channel.getThreadChannels().stream()
+                    .filter(t -> t.getId().equals(threadId))
+                    .findFirst()
+                    .orElse(null);
+
+            if (thread != null) {
+                thread.sendMessage(message).queue(messageQueue ->{
+                    messageQueue.addReaction(Emoji.fromUnicode("👍")).queue();
+                });
+            } else {
+                System.err.println("Thread non trovato!");
+            }
+        } else {
+            System.err.println("Canale non trovato!");
+        }
+    }
+}
